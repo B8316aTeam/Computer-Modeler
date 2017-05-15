2a6526b75659b7025a5ae0956c2d3a75527c30e7
KIVI <KIVI@DESKTOP-09903DK>
2017-05-15 16:08:21 +1000

    Сделана память

---
+++ Main/MachMem/Header.cpp
@@ -0,0 +1,49 @@
+#include "Specific.h"
+#include "Header.h"
+
+bool GetData(void * memory, unsigned id_cell, int * data)
+{
+	MEMORY * memory_ = (MEMORY *)memory;
+	if (memory_->size < id_cell)
+	{
+		*data = memory_->mem_[id_cell];
+		return false;
+	}
+	return true;
+}
+
+bool SetData(void * memory, unsigned id_cell, int data)
+{
+	MEMORY * memory_ = (MEMORY *)memory;
+	if (memory_->size < id_cell)
+	{
+		memory_->mem_[id_cell] = data;
+		return false;
+	}
+	return true;
+}
+
+bool InitProgram(void * memory, void * mach_source)
+{
+	MEMORY * memory_ = (MEMORY *)memory;
+	MACH_SOURCE * mach_source_ = (MACH_SOURCE *)mach_source;
+	if (memory_->size < mach_source_->size_)
+		return true;
+	for (unsigned i = 0; i < mach_source_->size_; i++)
+		memory_->mem_[i] = mach_source_->commands_[i];
+	return false;
+}
+
+void * Init(unsigned size)
+{
+	MEMORY * out = new MEMORY;
+	out->mem_ = new int[size];
+	out->size = size;
+	return (void *) out;
+}
+
+void DeleteMemory(void * memory)
+{
+	delete [] ((MEMORY *)memory)->mem_;
+	delete (MEMORY *)memory;
+}
--- Main/MachMem/Header.h
+++ Main/MachMem/Header.h
@@ -1,1 +1,6 @@
 #pragma once
+extern "C" __declspec(dllexport) bool GetData(void * memory, unsigned id_cell, int * data);
+extern "C" __declspec(dllexport) bool SetData(void * memory, unsigned id_cell, int data);
+extern "C" __declspec(dllexport) bool InitProgram(void * memory ,void * mach_source);
+extern "C" __declspec(dllexport) void * Init(unsigned size);
+extern "C" __declspec(dllexport) void DeleteMemory(void * memory);
